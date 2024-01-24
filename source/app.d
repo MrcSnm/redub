f4336d71a58d1e3cc1114d1fc303d2436fa3b788
Hipreme <msnmancini@hotmail.com>
2024-01-23 21:35:49 -0300

    Version bump

--- source/app.d
+++ source/app.d
@@ -14,7 +14,7 @@ import redub.tree_generators.dub;
 import redub.cli.dub;
 import redub.command_generators.commons;
 
-enum RedubVersion = "Redub - A reimagined DUB: v1.2.5";
+enum RedubVersion = "Redub - A reimagined DUB: v1.2.6";
 
 
 string formatError(string err)
