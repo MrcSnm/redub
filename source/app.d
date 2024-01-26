9677410345fa30f1661b1935b1c2f3e60fae93e0
MrcSnm <msnmancini@hotmail.com>
2024-01-25 23:11:03 -0300

    Bump version

--- source/app.d
+++ source/app.d
@@ -14,7 +14,7 @@ import redub.tree_generators.dub;
 import redub.cli.dub;
 import redub.command_generators.commons;
 
-enum RedubVersion = "Redub - A reimagined DUB: v1.2.9";
+enum RedubVersion = "Redub - A reimagined DUB: v1.2.11";
 
 
 string formatError(string err)
