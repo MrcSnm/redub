00962d0bb0c05dbff96d6d872361ad56a5b03fc5
Hipreme <msnmancini@hotmail.com>
2025-03-04 22:07:50 -0300

    Update: Improved memory footprint for JSONValue

--- source/redub/buildapi.d
+++ source/redub/buildapi.d
@@ -8,7 +8,7 @@ import redub.package_searching.api;
 
 
 ///vX.X.X
-enum RedubVersionOnly = "v1.21.9";
+enum RedubVersionOnly = "v1.21.10";
 ///Redub vX.X.X
 enum RedubVersionShort = "Redub "~RedubVersionOnly;
 ///Redub vX.X.X - Description
