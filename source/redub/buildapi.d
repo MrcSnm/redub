f372fffe5b5f65b9e3d0b25d5073aa0f02520a43
MrcSnm <msnmancini@hotmail.com>
2025-08-26 22:14:45 +0100

    Update: Added segmented hashmap to redub json

--- source/redub/buildapi.d
+++ source/redub/buildapi.d
@@ -8,7 +8,7 @@ import redub.package_searching.api;
 
 
 ///vX.X.X
-enum RedubVersionOnly = "v1.24.5";
+enum RedubVersionOnly = "v1.24.6";
 ///Redub vX.X.X
 enum RedubVersionShort = "Redub "~RedubVersionOnly;
 ///Redub vX.X.X - Description
