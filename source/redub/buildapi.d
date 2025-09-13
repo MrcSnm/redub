507c1e513b34f9ce2bba3b23d917bc1cfcececa0
Hipreme <msnmancini@hotmail.com>
2025-09-13 11:09:09 -0300

    Update: Build redub with the master version of redub

--- source/redub/buildapi.d
+++ source/redub/buildapi.d
@@ -8,7 +8,7 @@ import redub.package_searching.api;
 
 
 ///vX.X.X
-enum RedubVersionOnly = "v1.24.14";
+enum RedubVersionOnly = "v1.24.15";
 ///Redub vX.X.X
 enum RedubVersionShort = "Redub "~RedubVersionOnly;
 ///Redub vX.X.X - Description
